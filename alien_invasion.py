bb8369f8f559e55948d44f899eec72204ad6bafd
刘承远 <3214997296@qq.com>
2023-08-11 16:16:30 +0800

    Drive ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,16 +14,27 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.set_screen(self.settings.full_screen)
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+
+    def set_screen(self, switch):
+        """屏幕设置"""
+        if switch:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
+        else:
+            self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height))
+
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -32,6 +43,27 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按下"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应释放"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
--- settings.py
+++ settings.py
@@ -6,3 +6,7 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.full_screen = 0
+
+        # 飞船的设置
+        self.ship_speed = 1.5
